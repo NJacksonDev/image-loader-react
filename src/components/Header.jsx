27b05402d86dbe73439e5adf40e3e61b7a89fd3d
Nick Jackson <nickdjackson@msn.com>
2022-12-27 22:23:48 -0500

    additional edits

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header({ setUser }) {
   return (
     <div className="header">
       <header>Image Loader</header>
-      <div className="selected-header">
+      <div className="logout">
         <Logout setUser={setUser} />
       </div>
     </div>
