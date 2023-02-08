2059314eefef5e28c061e9896cb87012870480a0
Nick Jackson <nickdjackson@msn.com>
2023-02-07 20:11:34 -0500

    logout change

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -3,7 +3,14 @@ import { Button } from "antd";
 export default function Logout({ setUser }) {
   return (
     <div>
-      <Button onClick={() => setUser(null)} type="primary" size="large">
+      <Button
+        onClick={() => {
+          setUser(null);
+          localStorage.removeItem("user");
+        }}
+        type="primary"
+        size="large"
+      >
         Logout
       </Button>
     </div>
